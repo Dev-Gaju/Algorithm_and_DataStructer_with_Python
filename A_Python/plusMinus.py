ddfa2b2cda734f787af8a01b68517928c119913f
Dev-Gaju <gajuahmd@gmail.com>
2022-06-16 16:51:59 +0600

    check commit

--- A_Python/plusMinus.py
+++ A_Python/plusMinus.py
@@ -4,3 +4,21 @@ arr = [1 if int(temp)>0 else -1 if int(temp)<0 else 0 for temp in input().strip(
 print("{0:.6f}".format(arr.count(1)/n))
 print("{0:.6f}".format(arr.count(-1)/n))
 print("{0:.6f}".format(arr.count(0)/n))
+
+
+
+
+# p = 0
+# n = 0
+# z = 0
+# for i in range(len(values)):
+#     if values[i]  < 0:
+#         n +=1
+#     elif values[i] > 0:
+#         p += 1
+#     else:
+#         z +=1
+#
+# print("{0:6f}".format(p/len(values)))
+# print("{0:6f}".format(n/len(values)))
+# print("{0:6f}".format(z/len(values)))
