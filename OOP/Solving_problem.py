c76b9618e432382668e63da91b95a4cb3bfb0552
Dev-Gaju <gajuahmd@gmail.com>
2022-06-25 23:23:35 +0600

    check update

--- OOP/Solving_problem.py
+++ OOP/Solving_problem.py
@@ -1,15 +1,3 @@
-a = []
-for i in range(5):
-    ele = int(input())
-    a.append((ele))
-# print(a)
-
-a.sort()
-b=0
-for i in range(len(a)-1):
-     b += a[i]
-print(b)
-c=0
-for i in range(1,len(a)):
-     c += a[i]
-print(c)
+x=5
+nums = [int(x) for x in input().strip().split(' ')]
+print(sum(nums) - max(nums), sum(nums) - min(nums))
